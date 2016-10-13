3f775adeeb0a4d906e20972b7ae1903b09c3a966
Andrew Paseltiner <apaseltiner@gmail.com>
2016-10-13 11:28:56 -0400

    Discard gh-pages commit history

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -8,11 +8,9 @@ git init
 git config user.email 'apaseltiner@gmail.com'
 git config user.name 'Andrew Paseltiner'
 git remote add upstream "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
-git fetch upstream gh-pages
-git reset upstream/gh-pages
 
 touch .
 
 git add -A .
-git commit -m "rebuild pages at ${TRAVIS_TAG}"
-git push -q upstream HEAD:gh-pages
+git commit -qm "rebuild pages at ${TRAVIS_TAG}"
+git push -q upstream HEAD:gh-pages --force
